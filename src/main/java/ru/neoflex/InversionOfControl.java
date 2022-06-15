59568f938f906af2f3ab155544a16ef5bfacefcf
Hollowflk <Hollowflk@gmail.com>
2022-06-15 20:58:35 +0300

    Initial commit

---
+++ src/main/java/ru/neoflex/InversionOfControl.java
@@ -0,0 +1,20 @@
+package ru.neoflex;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.neoflex.repository.OrderRepository;
+import ru.neoflex.service.OrderService;
+
+/**
+ * InversionOfControl.
+ *
+ * @author nanikeev
+ */
+public class InversionOfControl {
+
+    public static void main(String... args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("context-config.xml");
+        OrderService orderService = context.getBean(OrderService.class);
+        orderService.createNew(1L);
+    }
+}
